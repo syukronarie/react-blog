674e87d04f53bd30f155d0955a58289c27c08922
syukronarie <syukronarie@gmail.com>
2022-11-30 18:38:34 +0700

    refactor(code): clear unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/jsx-no-useless-fragment */
-// lib
 import { ConfigProvider } from 'antd';
 import { ErrorBoundary } from 'react-error-boundary';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 // routes
@@ -33,51 +30,15 @@ const myErrorHandler = (error) => {
   }
 };
 
-const CountState = ({ state }) => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    return () => {
-      setCount(0);
-      console.log('Unmounting');
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log({ count });
-    if (state) setCount(state);
-  }, [state, count]);
-
-  return <p>{count}</p>;
-};
-
 function App() {
-  // const element = useRoutes(routes);
-
-  const [state, setState] = useState(0);
-  const [hide, setHide] = useState(false);
-
-  useEffect(() => {
-    console.log('mounting');
-  }, []);
-
-  useEffect(() => {
-    console.log('updating');
-    console.log({ state });
-  }, [state]);
+  const element = useRoutes(routes);
 
   return (
-    <>
-      <p>test</p>
-      <button onClick={() => setState(state + 1)}>Increment</button>
-      <button onClick={() => setHide(!hide)}>Hide Result Count</button>
-      {!hide && <CountState state={state} />}
-    </>
-    // <ConfigProvider>
-    //   <ErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
-    //     {element}
-    //   </ErrorBoundary>
-    // </ConfigProvider>
+    <ConfigProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
+        {element}
+      </ErrorBoundary>
+    </ConfigProvider>
   );
 }
 
